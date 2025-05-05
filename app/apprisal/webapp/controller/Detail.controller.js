d53ede96373ed20992bbbd7225b28a614aafd1d8
Joy Jibin <jibin@maventic.com>
2025-05-05 10:02:34 +0000

    Detail Side Navgaition Added

--- app/apprisal/webapp/controller/Detail.controller.js
+++ app/apprisal/webapp/controller/Detail.controller.js
@@ -5,6 +5,29 @@ sap.ui.define([
 
     return Controller.extend("apprisal.controller.Detail", {
         onInit() {
+        },
+        onSideNavigationExpand(){
+            const oSideNavigation = this.byId("sideNavigation"),
+				bExpanded = oSideNavigation.getExpanded();
+
+			oSideNavigation.setExpanded(!bExpanded);
+
+            const oSplitContDemo = this.byId("SplitContDemo");
+            if(bExpanded)
+            {
+                oSplitContDemo.addStyleClass("mvnSplitContainer");
+            }   
+            else
+            {
+                oSplitContDemo.removeStyleClass("mvnSplitContainer");
+            } 
+
+        },
+        onSideNavigationItemSelect(oEvent){
+            
+            this.byId("SplitContDemo").toDetail(this.byId( oEvent.getParameter("item").getKey()))
+            // this._initalSelectedKey = ;
+            // this.byId( this._initalSelectedKey).setVisible(true);
         }
     });
 });
