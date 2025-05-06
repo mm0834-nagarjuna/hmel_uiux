1f0618339b12f29fa833b50f0806beb490897d4f
Joy Jibin <jibin@maventic.com>
2025-05-06 18:14:46 +0000

    Commit 4

---
+++ app/apprisal/webapp/controller/Idp.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    
+], (Controller,GroupHeaderListItem) => {
+    "use strict";
+
+    return Controller.extend("apprisal.controller.Idp", {
+        onInit() {
+           
+        }
+        
+
+
+    });
+});
