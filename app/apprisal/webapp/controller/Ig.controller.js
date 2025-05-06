c3cb4cc9ad07dd3ad14e863d186bf9d8dfe17f7a
Mridupaban Bhattacharjya <mridupaban@maventic.com>
2025-05-06 19:13:33 +0000

    added Table fragment for Prev_year Indv_Goals

---
+++ app/apprisal/webapp/controller/Ig.controller.js
@@ -0,0 +1,38 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/ui/core/Fragment",
+], (Controller,MessageBox,Fragment) => {
+    "use strict";
+
+    return Controller.extend("apprisal.controller.Ig", {
+        onInit() {
+           
+        },
+        
+        onClickCancel: function(){
+
+        },
+
+        onClickPreviousYear: function (oEvent) {
+            var oButton = oEvent.getSource(),
+            oView = this.getView();
+
+            if (!this._pDialog) {
+                this._pDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "apprisal.fragment.IgPreYear",
+                    controller: this
+                }).then(function(oDialog){
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            this._pDialog.then(function(oDialog){
+                oDialog.setMultiSelect(true);
+                oDialog.open();
+            }.bind(this));
+        }
+    });
+});
