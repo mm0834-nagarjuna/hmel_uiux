1284a1217aa41fd1b096b54299430826ca13f861
Mridupaban Bhattacharjya <mridupaban@maventic.com>
2025-05-07 19:54:00 +0000

    Bcf completed

--- app/apprisal/webapp/controller/Bcf.controller.js
+++ app/apprisal/webapp/controller/Bcf.controller.js
@@ -1,8 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/GroupHeaderListItem"
-    
-], (Controller,GroupHeaderListItem) => {
+    "sap/m/GroupHeaderListItem",
+    "sap/ui/core/Fragment",    
+], (Controller,GroupHeaderListItem,Fragment) => {
     "use strict";
 
     return Controller.extend("apprisal.controller.Cof", {
@@ -22,9 +22,38 @@ sap.ui.define([
 
         onBeforeRendering : function(){
             this.byId("BcfTable").setSticky(["ColumnHeaders"]);
-        }
+        },
 
-        
+         // ----Function for calling Score Infor Dialog--------
+
+         onClickScoreInfo: function (oEvent) {
+            console.log('getting called')
+            var oButton = oEvent.getSource(),
+            oView = this.getView();
+
+            if (!this._pDialog) {
+                this._pDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "apprisal.fragment.Bce_score",
+                    controller: this
+                }).then(function(oDialog){
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            this._pDialog.then(function(oDialog){
+                // oDialog.setMultiSelect(true);
+                oDialog.open();
+            }.bind(this));
+        },
+        onClickCancelScoreInfo: function () {
+            var oView = this.getView();
+            var oDialog = oView.byId("idBceScoreInfoDialog");
+            if (oDialog) {
+                oDialog.close();
+            }
+        }
 
 
     });
