06928453955c1e8d355793ab389a79721a357d42
Mridupaban Bhattacharjya <mridupaban@maventic.com>
2025-05-07 06:32:27 +0000

    removed commented codes from Bcf&Cof controllers

--- app/apprisal/webapp/controller/Cof.controller.js
+++ app/apprisal/webapp/controller/Cof.controller.js
@@ -34,56 +34,6 @@ sap.ui.define([
                 oDialog.open();
             }.bind(this));
         }
-        
-
-        //2nd trail for dialog binding
-        // onPreviousYearIG2: function (oEvent) {
-        //     oView = this.getView();
-        //     if (!this._pDialog) {
-		// 		this._pDialog = Fragment.load({
-		// 			id: "dialog",
-		// 			name: "apprisal.view.IgDialog",
-		// 			controller: this
-		// 		}).then(function(oDialog){
-        //             oDialog.setModel(oView.getModel("CustomData"));
-        //             oDialog.open();
-        //         }.bind(this));
-		// 	}
-        // },
-        // Bttest:function(){
-        //     console.log("Hi");
-        // }
-        // ,
-
-// Select Dialog
-        // onSelectDialogPress: function (oEvent) {
-		// 	var oButton = oEvent.getSource(),
-		// 		oView = this.getView();
-		// 	if (!this._pDialog) {
-		// 		this._pDialog = Fragment.load({
-		// 			id: oView.getId(),
-		// 			name: "apprisal.view.IgDialog",
-		// 			controller: this
-		// 		}).then(function(oDialog){
-        //             oDialog.setModel(oView.getModel("PreDefineGoals"));
-        //             oDialog.open();
-        //         }.bind(this));
-		// 	}
-
-
-		// },
-
-		// _configDialog: function (oButton, oDialog) {
-		// 	// Multi-select if required
-		// 	var bMultiSelect = !!oButton.data("multi");
-		// 	oDialog.setMultiSelect(bMultiSelect);
-
-		// 	var sCustomConfirmButtonText = oButton.data("confirmButtonText");
-		// 	oDialog.setConfirmButtonText(sCustomConfirmButtonText);
-		// },
-
-        
-
 
     });
 });
