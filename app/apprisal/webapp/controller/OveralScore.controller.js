1dbfd495529eca64e9cd7026aeee94ba8135b392
Nagarjuna Posa <nagarjuna@maventic.com>
2025-05-10 06:53:17 +0000

    Overall Score Link Dialog completed

--- app/apprisal/webapp/controller/OveralScore.controller.js
+++ app/apprisal/webapp/controller/OveralScore.controller.js
@@ -1,12 +1,43 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"    
     
-], (Controller,GroupHeaderListItem) => {
+], (Controller,Fragment) => {
     "use strict";
 
     return Controller.extend("apprisal.controller.OveralScore", {
         onInit() {
            
+        },
+        handlePopoverPress: function (oEvent) {
+            let oControl = oEvent.getSource(); 
+            let oText = oControl.getText();   
+            let oView = this.getView();
+        
+           
+            if (oText && oText.includes("BSM Score")) {
+                if (!this._pDialog) {
+                    this._pDialog = Fragment.load({
+                        id: oView.getId(),
+                        name: "apprisal.fragment.OS_section_popOver",
+                        controller: this
+                    }).then(function(oDialog) {
+                        oView.addDependent(oDialog);
+                        return oDialog;
+                    });
+                }
+        
+                this._pDialog.then(function(oDialog) {
+                    oDialog.open();
+                }.bind(this));
+            } else {
+                
+                console.log("Link does not contain 'BSM Score'. Dialog not opened.");
+            }
+        }
+        ,
+        handleActionPress : function () {
+            this.byId('myPopover').close()
         }
         
 
