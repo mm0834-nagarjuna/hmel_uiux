b7bd9009f25c32b73c9a80f51d05daa1c28607bb
Mridupaban Bhattacharjya <mridupaban@maventic.com>
2025-05-10 14:42:10 +0000

    Name are maed in bold for all 3 sections in Scorecard

--- app/apprisal/webapp/controller/Scorecard.controller.js
+++ app/apprisal/webapp/controller/Scorecard.controller.js
@@ -1,13 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+
 ], (Controller) => {
     "use strict";
 
     return Controller.extend("apprisal.controller.Scorecard", {
         onInit() {
-           	// HTML string bound to the formatted text control
-               var oModel = new sap.ui.model.json.JSONModel({
-                HTML: "Scorecard of <span style='font-weight: bold;'>Girish Chandra Ghildiyal ( 100682 )</span>"
+            var oModel = new sap.ui.model.json.JSONModel({
+                Girish: "Scorecard of <span style='font-weight: bold;'>Girish Chandra Ghildiyal ( 100682 )</span>",
+                Partha:  "Scorecard of <span style='font-weight: bold;'>Partha Chakraborty ( 100785 )</span>",
+                Kushagra:   "Scorecard of <span style='font-weight: bold;'>Kushagra Verma ( 100056 )</span>"
             });
             this.getView().setModel(oModel, "scoreModel"); 
         }
