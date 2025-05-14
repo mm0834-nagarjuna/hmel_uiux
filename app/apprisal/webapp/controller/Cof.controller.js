b94a980a769ca77b6f988b4fe262783fe6743830
Nagarjuna Posa <nagarjuna@maventic.com>
2025-05-14 04:42:59 +0000

    Tree hBox

--- app/apprisal/webapp/controller/Cof.controller.js
+++ app/apprisal/webapp/controller/Cof.controller.js
@@ -12,6 +12,7 @@ sap.ui.define([
             var oData = {
                 "COF": {
                     "KSP001": {
+
                         "nodes":
                             [
                                 {
@@ -20,50 +21,62 @@ sap.ui.define([
                                     "text": "Key stratagic Priority",
                                     "add": "sap-icon://add",
                                     "addText": 'Add KRA',
+
                                     "delete": "sap-icon://delete",
                                     "visible": true,
                                     "editable": true,
-                                    "isTA":true,
-                                    "isDrpD":false,
+                                    "isTA": true,
+                                    "isDrpD": false,
                                     "nodes": [
                                         {
                                             "Key": "KRA",
                                             "value": "Key Result Area value",
                                             "text": "Key Result Area",
                                             "add": null,
+                                            "addText": null,
+
+                                            "delete": null,
                                             "delete": null,
                                             "visible": true,
                                             "editable": true,
-                                            "isTA":true,
-                                            "isDrpD":false
+                                            "isTA": true,
+                                            "isDrpD": false
                                         },
                                         {
                                             "Key": "KPI",
                                             "value": "Key Performance Index value",
                                             "text": "Key Performance Index",
                                             "add": null,
+                                            "addText": null,
+
+                                            "delete": null,
                                             "delete": null,
                                             "visible": true,
                                             "editable": true,
-                                            "isTA":true,
-                                            "isDrpD":false
+                                            "isTA": true,
+                                            "isDrpD": false
                                         },
                                         {
                                             "Key": "Target",
                                             "value": "Target value",
                                             "text": "Annual Target",
                                             "add": null,
+                                            "addText": null,
+
+                                            "delete": null,
                                             "delete": null,
                                             "visible": true,
                                             "editable": true,
-                                            "isTA":true,
-                                            "isDrpD":false
+                                            "isTA": true,
+                                            "isDrpD": false
                                         },
                                         {
                                             "Key": "FFMmilestone",
                                             "value": "First Four Monthly Milestones value",
                                             "text": "First Four Monthly Milestones",
                                             "add": null,
+                                            "addText": null,
+                                            "delete": null,
                                             "delete": null,
                                             "visible": true,
                                             "editable": true
@@ -73,22 +86,26 @@ sap.ui.define([
                                             "value": "Second Four Monthly Milestones value",
                                             "text": "Second Four Monthly Milestones",
                                             "add": null,
+                                            "addText": null,
+                                            "delete": null,
                                             "delete": null,
                                             "visible": true,
                                             "editable": true
                                         },
-                                        
+
 
                                         {
                                             "Key": "FFMA:AE",
                                             "value": "First Four Monthly Achievement: Appraisee Value",
                                             "text": "First Four Monthly Achievement: Appraisee",
                                             "add": null,
+                                            "addText": null,
+                                            "delete": null,
                                             "delete": null,
                                             "visible": true,
                                             "editable": true,
-                                            "isTA":true,
-                                            "isDrpD":false
+                                            "isTA": true,
+                                            "isDrpD": false
                                         }
                                         ,
 
@@ -97,49 +114,54 @@ sap.ui.define([
                                             "value": "First Four Monthly Feedback: Appraiser Value",
                                             "text": "First Four Monthly Feedback: Appraiser",
                                             "add": null,
+                                            "addText": null,
+                                            "delete": null,
                                             "delete": null,
                                             "visible": true,
                                             "editable": true,
-                                            "isTA":true,
-                                            "isDrpD":false
+                                            "isTA": true,
+                                            "isDrpD": false
                                         },
                                         {
                                             "Key": "Weightage",
                                             "value": "5",
                                             "text": "Weightage  (%)",
                                             "add": null,
+                                            "addText": null,
+                                            "delete": null,
                                             "delete": null,
                                             "visible": true,
                                             "editable": true,
-                                            "isTA":false,
-                                            "isDrpD":true
+                                            "isTA": false,
+                                            "isDrpD": true
                                         },
                                         {
+                                            "field": "Appraiser_Score",
                                             "Key": "AS",
                                             "value": "5",
                                             "text": "Appraisee Score",
                                             "add": 'sap-icon://information',
-                                            "addText":null,
-                                            "addPress":'onClickScoreInfo',
+                                            "addText": null,
+                                            "addPress": 'onClickScoreInfo',
                                             "delete": null,
-                                            "info":'sap-icon://information',
                                             "visible": true,
                                             "editable": true,
-                                            "isTA":false,
-                                            "isDrpD":true
+                                            "isTA": false,
+                                            "isDrpD": true
                                         },
                                         {
+                                            "field": "Appraiser_Score",
                                             "Key": "AS",
                                             "value": "5",
                                             "text": "Appraiser Score",
                                             "add": 'sap-icon://information',
-                                            "addText":null,
+                                            "addText": null,
+                                            "addPress": 'onClickScoreInfo',
                                             "delete": null,
-                                            "info":'sap-icon://information',
                                             "visible": true,
                                             "editable": true,
-                                            "isTA":false,
-                                            "isDrpD":true
+                                            "isTA": false,
+                                            "isDrpD": true
                                         },
 
                                     ]
@@ -168,7 +190,7 @@ sap.ui.define([
             this.getView().setModel(oModel, "myGoal");
 
             const oRouter = this.getOwnerComponent().getRouter();
-			oRouter.getRoute("apprisalDetail").attachPatternMatched(this.onObjectMatched, this);
+            oRouter.getRoute("apprisalDetail").attachPatternMatched(this.onObjectMatched, this);
 
         },
 
@@ -260,14 +282,14 @@ sap.ui.define([
                 return false
 
             },
-            isAS:function(oText){
+            isAS: function (oText) {
                 if (oText === 'Weightage') {
                     return false
                 }
                 if (oText === 'AS') {
                     return true
                 }
-                
+
                 return false
             },
             isTA: function (oText) {
@@ -283,7 +305,7 @@ sap.ui.define([
                 }
                 return true
             },
-            functionPress: function(oText){
+            functionPress: function (oText) {
                 console.log(oText)
             }
 
@@ -292,40 +314,50 @@ sap.ui.define([
 
             var that = this;
             const oModel = this.getView().getModel("apprisalData"); // default OData V4 model
-            const oContextBinding = oModel.bindContext("/appraisal(index="+oEvent.getParameter("arguments").index+",appraisalId="+oEvent.getParameter("arguments").appId+")");
+            const oContextBinding = oModel.bindContext("/appraisal(index=" + oEvent.getParameter("arguments").index + ",appraisalId=" + oEvent.getParameter("arguments").appId + ")");
 
             console.log(oContextBinding)
-			oContextBinding.requestObject().then(function (oData) {
-                var aAck  = [];
+            oContextBinding.requestObject().then(function (oData) {
+                var aAck = [];
                 var _aData = that._aData;
                 console.log(_aData)
-                if (oData.ackTSC)
-                {   
-                    _aData[0].Editable =  oData.ackTSCEdit;  
+                if (oData.ackTSC) {
+                    _aData[0].Editable = oData.ackTSCEdit;
                     aAck.push(_aData[0]);
-                }   
-                if (oData.ackFFMC)
-                {   
-                    _aData[1].Editable =  oData.ackFFMCEdit;  
+                }
+                if (oData.ackFFMC) {
+                    _aData[1].Editable = oData.ackFFMCEdit;
                     aAck.push(_aData[1]);
-                } 
-                if (oData.ackSFMC)
-                {   
-                    _aData[2].Editable =  oData.ackSFMCEdit;  
+                }
+                if (oData.ackSFMC) {
+                    _aData[2].Editable = oData.ackSFMCEdit;
                     aAck.push(_aData[2]);
-                } 
-                if (oData.ackANC)
-                {   
-                    _aData[3].Editable =  oData.ackANCEdit;  
+                }
+                if (oData.ackANC) {
+                    _aData[3].Editable = oData.ackANCEdit;
                     aAck.push(_aData[3]);
-                } 
+                }
 
-                that.getView().getModel("PreDefineGoal").setProperty("/Acknowledgements",aAck);
+                that.getView().getModel("PreDefineGoal").setProperty("/Acknowledgements", aAck);
 
-              }).catch(function (oError) {
+            }).catch(function (oError) {
                 console.error("Failed to load product:", oError);
-              });
-		}
-        
+            });
+        },
+        onBtnPress: function (oEvent){
+            var oButton = oEvent.getSource(),
+                oView = this.getView();
+
+            var sField = oButton.data("field");  
+            switch (sField) {
+                case 'Appraiser_Score':
+                    this.onClickScoreInfo()
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+
     });
 });
