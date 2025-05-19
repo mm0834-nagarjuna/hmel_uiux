3cef8c49a0393d0b801e8757063298fb5228d641
Yash Sharma <yash@maventic.com>
2025-05-19 08:05:29 +0000

    historic data workflow changes

--- app/apprisal/webapp/controller/List.controller.js
+++ app/apprisal/webapp/controller/List.controller.js
@@ -1,10 +1,37 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
+], (Controller, Fragment) => {
     "use strict";
 
     return Controller.extend("apprisal.controller.List", {
         onInit() {
+        },
+        onPressHistoricData: function (oEvent) {
+            var oView = this.getView();
+
+            if (!this._pDialog) {
+                this._pDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "apprisal.fragment.HistoricData",
+                    controller: this
+                }).then(function(oDialog){
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            this._pDialog.then(function(oDialog){
+                // oDialog.setMultiSelect(true);
+                oDialog.open();
+            }.bind(this));
+        },
+        onClickHistoricDataCancel: function (oEvent) {
+            if (this._pDialog) {
+                this._pDialog.then(function(oDialog){
+                     oDialog.close();
+                 }.bind(this));
+             }
         },
         onListItemPress: function (oEvent) {
               var oListItem = oEvent.getSource();
@@ -23,15 +50,15 @@ sap.ui.define([
             let oTable = this.getView().byId('idApprisalTable')
             let btns = this.getView().byId('bts')
             console.log(sKey)
-            if(sKey === '03' || sKey === '02'){
+            // if(sKey === '03' || sKey === '02'){
                
                 
-                btns.setProperty("visible", true)
-                oTable.setProperty("mode", "SingleSelectLeft")
-            } else {
-                btns.setProperty("visible", false)
-                oTable.setProperty("mode", "None")
-            }
+            //     btns.setProperty("visible", true)
+            //     oTable.setProperty("mode", "SingleSelectLeft")
+            // } else {
+            //     btns.setProperty("visible", false)
+            //     oTable.setProperty("mode", "None")
+            // }
         }
         
     });
